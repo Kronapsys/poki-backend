1449238da14822342fde7577dfd07ebf27665d45
Kronapsys <rsxvii@gmail.com>
2021-04-28 17:58:59 +0200

    created lack_of_assistance controller

---
+++ controllers/lack-of-assistance.js
@@ -0,0 +1,50 @@
+const lack_of_assistance = require('../models/LackOfAssistance');
+
+const controller = { 
+    create: async (req, res) => {
+        try{
+            const item = await lack_of_assistance.create(req.body)
+            res.send(item)
+        }catch(err){
+            res.status(400).send(err)
+        }
+    },
+
+    read: async (req, res) => {
+        try{
+            const items = await lack_of_assistance.find({})  
+            res.send(items)
+        }catch(err){
+            res.status(400).send(err)
+        }
+    },
+
+    readById: async (req, res) => {
+        try{
+            const item = await lack_of_assistance.findById(req.params.id)  
+            return !item ? res.sendStatus(404) : res.send(item);
+        }catch(err){
+            res.status(400).send(err)
+        }
+    },
+
+    updateById: async (req, res) => {
+        try{
+            const item = await lack_of_assistance.findByIdAndUpdate(req.params.id, req.body, {new: true}) // {new: true} -> Trae el objeto ya actualizado
+            return !item ? res.sendStatus(404) : res.send(item);
+        }catch(err){
+            res.status(400).send(err)
+        }
+    },
+
+    deleteById: async (req, res) => {
+        try{
+            const item = await lack_of_assistance.findByIdAndDelete(req.params.id)
+            return !item ? res.sendStatus(404) : res.sendStatus(200);
+        }catch(err){
+            res.status(400).send(err)
+        }
+    }
+}
+
+module.exports = controller;
