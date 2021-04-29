271f68b3e5eb968f2778e26d8abd3bdbda74e0f6
Kronapsys <rsxvii@gmail.com>
2021-04-29 13:43:13 +0200

    created event controller

---
+++ controllers/event.js
@@ -0,0 +1,50 @@
+const event = require('../models/Event');
+
+const controller = { 
+    create: async (req, res) => {
+        try{
+            const item = await event.create(req.body)
+            res.send(item)
+        }catch(err){
+            res.status(400).send(err)
+        }
+    },
+        
+    read: async (req, res) => {
+        try{
+            const items = await event.find({})
+            res.send(items)
+        }catch(err){
+            res.status(400).send(err)
+        }
+    },
+
+    readById: async (req, res) => {
+        try{
+            const item = await event.findById(req.params.id)
+            return !item ? res.sendStatus(404) : res.send(item);
+        }catch(err){
+            res.status(400).send(err)
+        }
+    },
+
+    updateById: async (req, res) => {
+        try{
+            const item = await event.findByIdAndUpdate(req.params.id, req.body, {new: true}) // {new: true} -> Trae el objeto ya actualizado
+            return !item ? res.sendStatus(404) : res.send(item);
+        }catch(err){
+            res.status(400).send(err)
+        }
+    },
+
+    deleteById: async (req, res) => {
+        try{
+            const item = await event.findByIdAndDelete(req.params.id)
+            return !item ? res.sendStatus(404) : res.sendStatus(200);
+        }catch(err){
+            res.status(400).send(err)
+        }
+    }
+}
+
+module.exports = controller;
