bfcac59159fb548425f1b5ae4b15fa086b4915de
Kronapsys <rsxvii@gmail.com>
2021-04-29 10:59:14 +0200

    created allergy and allergyType routes and added to routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,8 @@ const teacherRoutes = require('./teachers');
 const childRoutes = require('./childs');
 const lack_of_assistanceRoutes = require('./lacks-of-assistance');
 const lack_of_assistance_typeRoutes = require('./lacks-of-assistance-type');
+const allergyRoutes = require('./allergies');
+const allergy_typeRoutes = require('./allergies-type');
 
 module.exports = {parentRoutes, teacherRoutes, childRoutes, lack_of_assistanceRoutes,
-                  lack_of_assistance_typeRoutes};
+                  lack_of_assistance_typeRoutes, allergyRoutes, allergy_typeRoutes};
