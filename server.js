0d8dd9ed86c9606b34a21a12ed333993036a77e3
Kronapsys <rsxvii@gmail.com>
2021-05-04 18:30:51 +0200

    added cors

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const app = express(); // Creamos el server
 
 const routerConfig = require('./utils/routerConfig');
 const routes = require('./routes/routes'); // Aquí importamos el fichero de routes.js así tenemos todas las rutas con un único import
+const cors = require('./middlewares/cors');
 
 app.use(morgan('dev')); // Utilizamos el middleware de morgan (nos trae información de las peticiones)
 app.use(express.json()); // Fuerza que todo sea tipo json
+app.use(cors); // Utilizamos las políticas de cors
 
 // Indicamos en que endpoint se ejecutará los métodos del router
 routerConfig(app, '/parents', routes.parentRoutes);
