aa52ce44b14e6dbf7c10bdbaed998cd9589a39cc
Kronapsys <rsxvii@gmail.com>
2021-05-10 23:53:57 +0200

    modified stoolReport model

--- models/StoolReport.js
+++ models/StoolReport.js
@@ -3,35 +3,42 @@ const Schema = mongoose.Schema;
 
 const StoolReportSchema = new Schema({
 
-    /*child: [{
+    child: [{
         type: Schema.Types.ObjectId,
         ref: 'Child'
-    }], */
+    }],
     
     type: [{
-        type: Schema.Types.ObjectId,
-        ref: 'StoolReportType'
+        // type: Schema.Types.ObjectId,
+        // ref: 'StoolReportType'
+        type: String,
+        require: true
     }],
 
     color: [{
-        type: Schema.Types.ObjectId,
-        ref: 'StoolReportColor'
+        // type: Schema.Types.ObjectId,
+        // ref: 'StoolReportColor'
+        type: String,
+        require: true
     }],
 
     blood: {
         type: Boolean,
+        default: false
     },
 
     parasites: {
         type: Boolean,
+        default: false
     },
 
     mucus: {
         type: Boolean,
+        default: false
     },
 
     description: {
-        type: String,
+        type: String
     }
 
 });
